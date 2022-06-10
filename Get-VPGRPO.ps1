dd2142b7b9687fe74507a283f06f2d9bf15fcca5
Tom Healy <95055678+tomhhealy@users.noreply.github.com>
2022-06-10 16:29:26 +0100

    Update Get-VPGRPO.ps1
    Resolved issue #1 in version 1.4 of the script, calling the logoff endpoint now works and no longer times PRTG out.

--- Get-VPGRPO.ps1
+++ Get-VPGRPO.ps1
@@ -1,25 +1,25 @@
 <#
 .SYNOPSIS
-Get-VPGRPO.ps1 retrieves information from the Zerto API hosted and maintained by My2Cloud. The intention of this script is to output all VPGs as individual channels and their current RPO to the PRTG Monitoring Platform.
- 
+Get-VPGStatus.ps1 retrieves information from the Zerto API hosted and maintained by My2Cloud. The intention of this script is to output all VPGs as individual channels and their current RPO to the PRTG Monitoring Platform.
+
 .DESCRIPTION
-Get-VPGRPO.ps1 is intended for use within the PRTG monitoring platform. Instructions for how the script should be configured are contained within my blog post.
+Get-VPGStatus.ps1 is intended for use within the PRTG monitoring platform. Instructions for how the script should be configured are contained within my blog post.
 The script itself must be placed in the following location - C:\Program Files (x86)\Paessler\PRTG Network Monitor\Custom Sensors\EXEXML\
 You must pass the username, password, zorgId and TenantId paramteres to the script in order for it to work.
 The OAuth2 Bearer token will be automatically generated every 8 hours and is stored in a variable.
- 
+
 .PARAMETER Username
 Represents your username OR email address used to login to the My2CLoud platform.
- 
+
 .PARAMETER Password
 Represents the password used to login to the My2Cloud platform. NOTE this value is visible inside the sensor settings in PRTG.
- 
+
 .PARAMETER TenandId
 Represents the My2Cloud Tenant ID.
- 
+
 .PARAMETER zorgId
 Represents the Zerto Organization ID.
- 
+
 .NOTES
 Author: Tom Healy
 Company: Total Computer Networks Ltd
@@ -29,8 +29,9 @@ Version History:
     1.1 23/01/2022  Updated script to now use command line parameters for interchangeable information such as zOrgs, 
     1.2 09/02/2022  Updated script to now call logoff endpoint via REST API due to session limit being reached.
     1.3 27/04/2022  Updated script to use a different LogOut endpoint via REST API to resolve issues with session limit.
+    1.4 06/06/2022  Updated script to include logoffInvoke last and then discard the output. This can still call the logoff endpoint so should no longer cause the sensor to error in PRTG.
 #>
- 
+
 # -- | Configured command line parameters | -- #
 [CmdletBinding()] param(
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $Username,
@@ -38,29 +39,22 @@ Version History:
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $tenantId,
     [Parameter(Mandatory = $true)][ValidateNotNullOrEmpty()][string] $zorgId
 )
- 
-# -- | My2Cloud - Authentication | -- #
+
+# -- | Authentication | -- #
 $authHeaders = @{'Abp.TenandId' = $tenantId}
 $authBody = '{"userNameOrEmailAddress": "' + $Username + '","password":"' + $Password + '"}'
 $authInvoke = Invoke-RestMethod -Uri "https://****CUSTOMER****.my2cloud.net:44301/api/TokenAuth/Authenticate" -Method POST -Body $authBody -ContentType 'application/json' -Headers $authHeaders
- 
- 
+
+
 # -- | My2Cloud - GetVpgs API Call | -- #
 $apiHeaders = @{"Authorization" = "Bearer " + $authInvoke.result.accessToken}
-$apiUri = "https://my2cloud.net:44301/api/services/app/ZertoService/GetVpgs?=" + $zorgId
+$apiUri = "https://****CUSTOMER****.my2cloud.net:44301/api/services/app/ZertoService/GetVpgs?zorgIdentifier=" + $zorgId
 $apiInvoke = Invoke-RestMethod -Uri $apiUri -Headers $apiHeaders -Method GET -ContentType 'application/json'
- 
-# -- | My2Cloud - LogOff API Call | -- #
-$logoffHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$logoffHeaders.Add("Accept", "*/*")
-$logoffHeaders.Add("Authorization", "Bearer 
-$logoffInvoke = Invoke-RestMethod 'https://****CUSTOMER****.my2cloud.net:44301/api/TokenAuth/LogOut' -Method 'GET' -Headers $logoffHeaders
-$logoffInvoke | ConvertTo-Json
- 
+
 # -- | Converts the results from the API call to useable format in PowerShell due to nested Json result | -- #
 $convertToJson = $apiInvoke.result.vpgs | ConvertTo-Json
 $vpgResults = $convertToJson | ConvertFrom-Json
- 
+
 # -- | Starts creating the XML output for PRTG unformatted | -- #
 $XML = "<PRTG>"
 foreach ($vpgResult in $vpgResults)
@@ -70,7 +64,7 @@ foreach ($vpgResult in $vpgResults)
     $XML += "<result><channel>$vpgName</channel><value>$vpgRpo</value><unit>TimeSeconds</unit><LimitMaxWarning>150</LimitMaxWarning><LimitMaxError>300</LimitMaxError></result>"
 }
 $XML += "</PRTG>"
- 
+
 # -- | Creates the XML writer to format the XML for PRTG to read | -- #
 function Out-XML ([xml]$XML)
 {
@@ -83,3 +77,14 @@ function Out-XML ([xml]$XML)
     Write-Output $StringWriter.ToString();
 }
 Out-XML "$XML"
+
+
+#$logoffHeaders.Add("Content-Type", "application/json")
+
+
+# -- | Call the logoff endpoint | -- #
+$logoffHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$logoffHeaders.Add("Accept", "*/*")
+$logoffHeaders.Add("Authorization", "Bearer $($authInvoke.result.accessToken)")
+$logoffInvoke = Invoke-RestMethod 'https://****CUSTOMER****.my2cloud.net:44301/api/TokenAuth/LogOut' -Method 'GET' -Headers $logoffHeaders
+$logoffInvoke | Out-Null
